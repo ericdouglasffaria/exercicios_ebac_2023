79da9438440ba5c8a0c8531548fc4b19cfca2834
ericdouglasffaria <ericdouglasffaria@hotmail.com.br>
2023-05-05 09:02:16 -0300

    Atualização do exercicos aula 11

--- Exercicios_11/cypress/e2e/02_login.cy.js
+++ Exercicios_11/cypress/e2e/02_login.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+const perfil = require('../fixtures/perfil.json');
 
 context('Funcionalidade Realizar login', () => {
   beforeEach(() => {
@@ -9,6 +10,29 @@ context('Funcionalidade Realizar login', () => {
     cy.screenshot();
   });
 
+  //-- EXEMPLO 01 - EXECUÇÃO TESTE FIXTURE --//
+  it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+    cy.fixture('perfil').then((dados) => {
+      cy.get('#username').type(dados.usuario, { log: false });
+      cy.get('#password').type(dados.senha, { log: false });
+      cy.get('.woocommerce-form > .button').click();
+      cy.get(
+        '.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)'
+      ).should('contain', 'Aluno');
+    });
+  });
+
+  //-- EXEMPLO 02 - EXECUÇÃO TESTE ARQUIVO DE DADOS --//
+  it('Deve fazer login com sucesso - Usando Arquivo de Dados', () => {
+    cy.get('#username').type(perfil.usuaruio);
+    cy.get('#password').type(perfil.senha);
+    cy.get('.woocommerce-form > .button').click();
+    cy.get(
+      '.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)'
+    ).should('contain', 'Aluno');
+  });
+
+  //-- EXEMPLO 03 - EXECUÇÃO TESTE FULL --//
   it('Deve fazer login com sucesso.', () => {
     cy.get('#username').type('ebacteste@gmail.com');
     cy.get('#password').type('!@alunoteste01!@');
--- Exercicios_11/cypress/support/commands.js
+++ Exercicios_11/cypress/support/commands.js
@@ -1,14 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
@@ -23,3 +12,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (usuario, senha) => {
+  cy.get('#username').type(usuario);
+  cy.get('#password').type(senha);
+  cy.get('.woocommerce-form > .button').click();
+});
